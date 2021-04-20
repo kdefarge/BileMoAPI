ed1d3865daf1860894ae0d429bdbf6b1b036a0e5
Kévin DEFARGE <1819360+kdefarge@users.noreply.github.com>
2021-04-20 05:57:37 +0200

    config jwt, add test, and update readme

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,8 +3,14 @@
 namespace App\Controller;
 
 use ApiPlatform\Core\Api\IriConverterInterface;
+use App\Repository\CustumerRepository;
+use Lexik\Bundle\JWTAuthenticationBundle\Security\Guard\JWTTokenAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 
 class SecurityController extends AbstractController
 {
@@ -38,5 +44,32 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/api/tokens", methods={"POST"})
+     */
+    public function newTokenAction(
+        Request $request,
+        CustumerRepository $custumerRepository,
+        PasswordEncoderInterface $encoder
+    ) {
+        $custumer = $custumerRepository->findOneBy(['email']);
+
+        if (!$custumer) {
+            throw $this->createNotFoundException('No custumer');
+        }
+
+        $isValid = $encoder->isPasswordValid(
+            $custumer->getPassword(),
+            $request->get('password'),
+            $custumer->getSalt()
+        );
+
+        if (!$isValid) {
+            throw new BadCredentialsException();
+        }
+
+        return new Response('TOKEN!');
     }
 }
